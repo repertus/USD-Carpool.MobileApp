bb1ecd21fff0814ca08443ff18c2a9d07873415a
Robert MartinDelCampo <robertmdelc@gmail.com>
2016-11-09 22:51:35 -0800

    Completed the google map route being displayed

--- www/app/main/pages/profile/routes.controller.js
+++ www/app/main/pages/profile/routes.controller.js
@@ -11,51 +11,76 @@
   function TripRouteController(tripRoutesFactory, $q, toastr, $scope) {
     var vm = this;
 
+    //Properties
+    vm.directionsService = new google.maps.DirectionsService;
+    vm.directionsDisplay = new google.maps.DirectionsRenderer;
+    var myLatlng = new google.maps.LatLng(32.771952, -117.188150);
+
+    //Methods
+    vm.calculateAndDisplayRoute = calculateAndDisplayRoute;
+
     activate();
 
     function activate() {
-         var myLatlng = new google.maps.LatLng(32.7157, -117.1611);
 
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 12,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-         vm.map = map;
+         //Renders the Google maps on the Route settings screen
 
 
 
+      var mapOptions = {
+        center: myLatlng,
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      vm.directionsDisplay.setMap(map);
 
+      vm.map = map;
+
+      //Calls the carpool routes from the server through the factory
       tripRoutesFactory.getAllRoutes().then(
         function(data) {
           vm.routes = data.routes;
         }
       );
 
-     //  google.maps.event.addDomListener(window, 'load', function() {
-     //    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+      //  google.maps.event.addDomListener(window, 'load', function() {
+      //    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
       //
-     //    var mapOptions = {
-     //      center: myLatlng,
-     //      zoom: 13,
-     //      mapTypeId: google.maps.MapTypeId.ROADMAP
-     //    };
+      //    var mapOptions = {
+      //      center: myLatlng,
+      //      zoom: 13,
+      //      mapTypeId: google.maps.MapTypeId.ROADMAP
+      //    };
       //
-     //    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      //    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
       //
-     //    navigator.geolocation.getCurrentPosition(function(pos) {
-     //      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-     //      var myLocation = new google.maps.Marker({
-     //        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-     //        map: map,
-     //        title: "My Location"
-     //      });
-     //    });
+      //    navigator.geolocation.getCurrentPosition(function(pos) {
+      //      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      //      var myLocation = new google.maps.Marker({
+      //        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+      //        map: map,
+      //        title: "My Location"
+      //      });
+      //    });
       //
-     //    $scope.map = map;
-     //  });
+      //    $scope.map = map;
+      //  });
+    }
+
+    function calculateAndDisplayRoute(route, directionsService, directionsDisplay)
+    {
+         directionsService.route({
+          origin: {lat: route.routeStartLat, lng: route.routeStartLong},
+          destination: {lat: route.routeEndLat, lng: route.routeEndLong},
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
     }
   }
 })();
