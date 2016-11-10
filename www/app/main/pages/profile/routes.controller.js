ac0e842cb7eddb57afb3ba71246d9e3fa247ff55
Robert MartinDelCampo <robertmdelc@gmail.com>
2016-11-09 22:06:58 -0800

    Setting up google maps

--- www/app/main/pages/profile/routes.controller.js
+++ www/app/main/pages/profile/routes.controller.js
@@ -1,24 +1,61 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .controller('TripRouteController', TripRouteController);
+  angular
+    .module('app')
+    .controller('TripRouteController', TripRouteController);
 
-    TripRouteController.$inject = ['tripRoutesFactory', '$q', 'toastr'];
+  TripRouteController.$inject = ['tripRoutesFactory', '$q', 'toastr', '$scope'];
 
-    /* @ngInject */
-    function TripRouteController(tripRoutesFactory, $q, toastr) {
-        var vm = this;
+  /* @ngInject */
+  function TripRouteController(tripRoutesFactory, $q, toastr, $scope) {
+    var vm = this;
 
-        activate();
+    activate();
 
-        function activate() {
-             tripRoutesFactory.getAllRoutes().then(
-                  function (routes) {
-                       vm.routes = routes;
-                  }
-             );
+    function activate() {
+         var myLatlng = new google.maps.LatLng(32.7157, -117.1611);
+
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+         vm.map = map;
+
+
+
+
+      tripRoutesFactory.getAllRoutes().then(
+        function(data) {
+          vm.routes = data.routes;
         }
+      );
+
+     //  google.maps.event.addDomListener(window, 'load', function() {
+     //    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+      //
+     //    var mapOptions = {
+     //      center: myLatlng,
+     //      zoom: 13,
+     //      mapTypeId: google.maps.MapTypeId.ROADMAP
+     //    };
+      //
+     //    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      //
+     //    navigator.geolocation.getCurrentPosition(function(pos) {
+     //      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+     //      var myLocation = new google.maps.Marker({
+     //        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+     //        map: map,
+     //        title: "My Location"
+     //      });
+     //    });
+      //
+     //    $scope.map = map;
+     //  });
     }
+  }
 })();
