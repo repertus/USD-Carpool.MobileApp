be07386018645867a0efaae54e3ad93e19e02825
Robert MartinDelCampo <robertmdelc@gmail.com>
2016-11-11 22:25:03 -0800

    Enabling user routes

--- www/app/main/pages/profile/routes.controller.js
+++ www/app/main/pages/profile/routes.controller.js
@@ -12,40 +12,46 @@
     var vm = this;
 
     //Properties
-    vm.directionsService = new google.maps.DirectionsService;
-    vm.directionsDisplay = new google.maps.DirectionsRenderer;
     var filterBarInstance;
+    var i = 0;
     var myLatlng = new google.maps.LatLng(32.771952, -117.188150);
 
     //Methods
+    vm.activeOriginationRoute = activeOriginationRoute;
     vm.calculateAndDisplayRoute = calculateAndDisplayRoute;
+    vm.directionsService = new google.maps.DirectionsService;
+    vm.directionsDisplay = new google.maps.DirectionsRenderer;
     vm.openModal = openModal;
     vm.profileId = $stateParams.profileId;
     vm.showFilterBar = showFilterBar;
 
-    /////////////////////////////////////////////////////////////////
-
     activate();
 
+    /////////////////////////////////////////////////////////////////
+
     function activate() {
-
-      //Renders the Google maps on the Route settings screen
-      var mapOptions = {
-        center: myLatlng,
-        zoom: 11,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      vm.directionsDisplay.setMap(map);
-
-      vm.map = map;
-
       //Calls the carpool routes from the server through the factory
       tripRoutesFactory.getAllRoutes().then(
         function(data) {
           vm.routes = data.routes;
         }
       );
+    }
+
+    function activeOriginationRoute(route) {
+         var index = vm.routes.indexOf(route);
+
+         for(i = 0; i < vm.routes.length; i++)
+         {
+              if(i != index && route.originationTrip == true)
+              {
+                   vm.routes[i].myStartRoute = false;
+              };
+         };
+
+     //     var vehicleStatusUpdate = {'_id' : vm.profileId, 'vehicle' : vm.vehicles};
+         //
+     //     vehicleFactory.activeVehicle(vehicleStatusUpdate).then();
     }
 
     //Displays the routes on the Google Maps screen
@@ -67,6 +73,18 @@
           window.alert('Directions request failed due to ' + status);
         }
       });
+    }
+
+    function googleMaps(){
+         var mapOptions = {
+           center: myLatlng,
+           zoom: 11,
+           mapTypeId: google.maps.MapTypeId.ROADMAP
+         };
+         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+         vm.directionsDisplay.setMap(map);
+
+         vm.map = map;
     }
 
     //Filters routes through the search bar in the modal per user input
@@ -101,6 +119,7 @@
     function openModal(index) {
       if (index == 1) $scope.oModal1.show();
       else $scope.oModal2.show();
+      googleMaps();
     };
 
     $scope.closeModal = function() {
@@ -115,16 +134,16 @@
     $scope.$on('$ionicView.enter', function() {
       vm.profile = sharedDataFactory.getProfile();
       var driver = vm.profile.driver;
-      var originationId = driver[0].originationId;
-      var returnId = driver[0].returnId;
+      vm.originationId = driver[0].originationId;
+      vm.returnId = driver[0].returnId;
 
-      tripRoutesFactory.getByRouteId(originationId).then(
+      tripRoutesFactory.getByRouteId(vm.originationId).then(
            function (originationRoute) {
                 vm.originationRoute = originationRoute.route;
            }
       );
 
-      tripRoutesFactory.getByRouteId(returnId).then(
+      tripRoutesFactory.getByRouteId(vm.returnId).then(
            function (returnRoute) {
                 vm.returnRoute = returnRoute.route;
            }
