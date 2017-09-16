a812ee54e6fe409339c5904bbfe42608b8f705e3
Robert MartinDelCampo <robertmdelc@gmail.com>
2017-09-15 20:14:34 -0700

    feat: Firebase configuration.

--- www/app/app.config.js
+++ www/app/app.config.js
@@ -3,6 +3,16 @@
 
   angular
     .module('app')
+    .config(['$firebase', function($firebase) {
+      $firebase.config({
+        apiKey: "AIzaSyB1i2swvgGsbNZylsIHFQfWSOjRkCQi1Tw",
+        authDomain: "usd-carpool.firebaseapp.com",
+        databaseURL: "https://usd-carpool.firebaseio.com",
+        projectId: "usd-carpool",
+        storageBucket: "usd-carpool.appspot.com",
+        messagingSenderId: "72591002426"
+      }); 
+    }])
     .run(function($ionicPlatform) {
       $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -16,6 +26,8 @@
           // org.apache.cordova.statusbar required
           StatusBar.styleDefault();
         }
-      });
-});
+      })
+
+      firebase.initializeApp($firebase.config);
+    });
 })();
